7db707c22649f3c411e69dcdc44c839424ba06f9
JohnTheMIner <xjohntheminer@gmail.com>
2019-10-09 22:05:58 -0700

    fixed comments

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,5 +1,34 @@
 package cse360assign2;
-
+/*+----------------------------------------------------------------------
+||
+||  Class AddingMachine 
+||
+||         Author:  John Lambert
+||			ID:		1214853159
+||        Purpose:  have an interger that can have values added to subtracted from 
+					it as well as have it cleared, displayed or show a history
+					of the processes that took place 
+||
+||  Inherits From:  NA
+||
+||     Interfaces:  NA
+||
+|+-----------------------------------------------------------------------
+||
+||      Constants:  NA
+||
+|+-----------------------------------------------------------------------
+||
+||   Constructors:  None
+||
+||  Class Methods:  getTotal returns the value of total
+					add adds a value to the total and adds the transaction to the string
+					subtract subtracts a value from the total and adds the transaction to the string
+					toString returns the list of transactions
+					clear clears the string and the total
+||
+||
+++-----------------------------------------------------------------------*/
 public class AddingMachine {
 
 	private int total;
